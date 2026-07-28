9f154eed825aa5f5c6ee0c9343d261e7c9499efe
dhruvcode84-cell <dhruv.code.84@gmail.com>
2026-07-28 15:54:52 +0530

    Update ar-scene.js

--- js/ar-scene.js
+++ js/ar-scene.js
@@ -41,7 +41,7 @@ const SCENE_LAYOUT = {
     scale: 1
   },
   flag: {
-    position: { x: -0.40, y: 0, z: 0.10 },
+    position: { x: -0.40, y: 1, z: 0.10 },
     rotation: { x: 0, y: 2.3, z: 0 },
     scale: 0.7
   },
