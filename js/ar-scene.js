64537c79e258a71b5e35d8e0e9dad688121f4379
dhruvcode84-cell <dhruv.code.84@gmail.com>
2026-07-29 12:03:57 +0530

    Update ar-scene.js

--- js/ar-scene.js
+++ js/ar-scene.js
@@ -36,7 +36,7 @@ const SCENE_LAYOUT = {
   //   scale: 1
   // },
   indiaGate: {
-    position: { x: -0.34, y: -0.28, z: -0.01 },
+    position: { x: -0.34, y: -0.28, z: 0 },
     rotation: { x: 0, y: 185, z: 0 },
     scale: 1
   },
